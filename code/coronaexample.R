0ad58edc479d002787cd5c8fd86b6b4cf1414e48
parksw3 <parksw3@mcmaster.ca>
2020-12-31 04:18:24 -0500

    push

--- code/coronaexample.R
+++ code/coronaexample.R
@@ -1,67 +1,62 @@
-library(tidyr)
-library(dplyr)
 library(ggplot2); theme_set(theme_bw())
+library(dplyr)
 library(gridExtra)
 
-source("makestuff/makeRfuns.R")
-commandEnvironments()
-## makeGraphics(width=8, height=3)
-makeGraphics()
+r <- log(2)/3
 
-## Generation intervals
-g_pre <- gen(sat=0*day)
-g_post <- gen(sat=10*day)
+genfun <- function(x, p=0.3, duration=2.5) {
+  p * (- pgamma(x, 4, rate=2/duration) + pgamma(x, 2, rate=2/duration))/duration +
+    (1-p) * (- pgamma(x, 6, rate=2/duration) + pgamma(x, 4, rate=2/duration))/duration
+}
 
-print(intervalMoments(g_pre))
-print(intervalMoments(g_post))
+pvec <- c(0.2, 0.4, 0.6, 0.8)
+tvec <- seq(0, 20, by=0.1)
 
-print(
-	ggplot(g_pre) +
-	geom_line(aes(time/day, density*day)) +
-	scale_x_continuous("Generation time (days)") +
-	scale_y_continuous("Density (per day)", expand=c(0, 0)) +
-	theme(
-		legend.position=c(0.9, 0.9),
-		legend.title=element_blank(),
-		panel.grid = element_blank(),
-		panel.border = element_blank(),
-		axis.line = element_line()
-	) +
-	geom_line(aes(time/day, density*day), data=g_post) +
-	NULL
-)
+genexample <- lapply(pvec, function(p) {
+  data.frame(
+    time=tvec,
+    genden=genfun(tvec, p),
+    p=paste0("p=", p)
+  )
+}) %>%
+  bind_rows
 
-## Interventions
+g1 <- ggplot(genexample) +
+  geom_line(aes(time, genden, lty=p)) +
+  scale_x_continuous("Generation time (days)") +
+  scale_y_continuous("Density (per days)", expand=c(0, 0)) +
+  ggtitle("A") +
+  theme(
+    legend.position=c(0.9, 0.9),
+    legend.title=element_blank(),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_line()
+  )
 
-base <- 0.1/day
+rRexample <- lapply(seq(0.01, 0.99, by=0.01), function(p) {
+  R <- 1/integrate(function(x) {
+    genfun(x, p) * exp(-r*x)
+  }, 0, Inf)[[1]]
+  
+  data.frame(
+    p=p, R=R
+  )
+}) %>%
+  bind_rows
 
-flat <- controlFun(base/2)
-trace <- controlFun(base, decay=1/4/day)
-respond <- controlFun(base, sat=4*day)
+g2 <- ggplot(rRexample) +
+  geom_line(aes(p, R)) +
+  scale_x_continuous("Proportion of pre-symptomatic transmission, p") +
+  scale_y_continuous("Reproduction number") +
+  ggtitle("B") +
+  theme(
+    legend.title=element_blank(),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_line()
+  )
 
-print(ggplot()
-	+ scale_x_continuous("Time since infection (days)")
-	+ scale_y_continuous("Control hazard (per day)", expand=c(0, 0))
-	+ geom_line(data=flat, aes(time/day, hazRate*day))
-	+ geom_line(data=trace, aes(time/day, hazRate*day))
-	+ geom_line(data=respond, aes(time/day, hazRate*day))
-)
+gtot <- grid.arrange(g1, g2, nrow=1)
 
-print(ggplot()
-	+ scale_x_continuous("Time since infection (days)")
-	+ scale_y_continuous("Control strength", expand=c(0, 0))
-	+ geom_line(data=flat, aes(time/day, strength))
-	+ geom_line(data=trace, aes(time/day, strength))
-	+ geom_line(data=respond, aes(time/day, strength))
-)
-
-## Intervention speed
-
-r0 <- 0.23/day
-
-phiFun(b0fun(g_pre, r0), flat)*day
-phiFun(b0fun(g_post, r0), flat)*day
-phiFun(b0fun(g_pre, r0), trace)*day
-phiFun(b0fun(g_post, r0), trace)*day
-phiFun(b0fun(g_pre, r0), respond)*day
-phiFun(b0fun(g_post, r0), respond)*day
+ggsave("coronaexample.pdf", gtot, width=8, height=4)
